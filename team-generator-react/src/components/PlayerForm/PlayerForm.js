10b907d36701b4ffa8e509420be1565df9137e20
Gareth Dobson <garethdobson@gmx.com>
2019-12-26 14:27:03 +0000

    Storing player names in forms local state

--- team-generator-react/src/components/PlayerForm/PlayerForm.js
+++ team-generator-react/src/components/PlayerForm/PlayerForm.js
@@ -3,17 +3,41 @@ import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 class PlayerForm extends Component {
+
+     constructor(props) {
+          super(props);
+          this.state = { 
+               name: '',
+               players: [],
+          };
+          this.handleChangeName = this.handleChangeName.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+     }
+
+     handleChangeName(e) {
+          this.setState({ name: e.currentTarget.value });
+     }
+
+     handleSubmit(e){
+          e.preventDefault();
+          this.setState({
+               players: [...this.state.players, this.state.name]
+          });
+     }
+     
      render() {
           return (
                <>
-                    <Form className="container">
+                    <Form className="container" onSubmit={ this.handleSubmit }>
                          <Form.Group controlId="playerName">
                               <h2>Add Players</h2>
                               <div className="d-flex">
                                    <Form.Control 
                                         type="text" 
                                         placeholder="Enter name" 
-                                        className="m-1" 
+                                        className="m-1"
+                                        value={ this.state.name } 
+                                        onChange={ this.handleChangeName }  
                                    />
                                    <Button 
                                         variant="outline-dark" 
