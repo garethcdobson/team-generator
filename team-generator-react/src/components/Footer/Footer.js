8ea0fdb7b80a0356a3562a3cd7467ea1c688ec63
Gareth Dobson <garethdobson@gmx.com>
2020-01-05 16:51:10 +0000

    Added footer info and links, and footer CSS styling

--- team-generator-react/src/components/Footer/Footer.js
+++ team-generator-react/src/components/Footer/Footer.js
@@ -3,6 +3,8 @@ import React from "react";
 const Footer = () => (
      <footer className="footer">
           <p>Created by Gareth Dobson 2019.</p>
+          <p>View repo on <a href="https://github.com/garethcdobson/team-generator">Github.</a></p>
+          <p>Final project at <a href="https://developme.training/fellowship/">DevelopMe.</a></p>
      </footer>
 );
 
